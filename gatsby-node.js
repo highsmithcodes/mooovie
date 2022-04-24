2b5b240d1438466cd708ac3f652704c5fb8d120d
highsmithcodes <veehighsmith@gmail.com>
2022-04-24 16:12:26 -0600

    remove dummy data

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,20 +1,9 @@
 const path = require('path')
-const data = require('./src/data/pageData')
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.createPages = async ({ actions, graphql }) => {
     const {createPage} = actions
 
-    data.forEach(page => {
-        createPage({
-            path: page.slug,
-            component: path.resolve('./src/templates/Generic.js'),
-            context: {
-                title: page.title,
-                description: page.description,
-            }
-        })
-    })
 
     const mdPages = await graphql(`
         query {
